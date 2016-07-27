fc130746b8ad6c6e5750d70e2275052455bb9878
Sean Henry <hello@seanhenry.codes>
2016-07-27 22:22:01 +0100

    Adds HTTPRequest

---
+++ Sources/network/HTTPRequest.swift
@@ -0,0 +1,25 @@
+//
+//  HTTPRequest.swift
+//
+//
+//
+
+import Foundation
+
+typealias URL = String
+enum HTTPMethod {
+    case get
+    case post
+}
+
+struct HTTPRequest: Equatable {
+    var url: URL
+    var method: HTTPMethod
+    var jsonBody: [String: AnyObject]
+}
+
+func ==(lhs: HTTPRequest, rhs: HTTPRequest) -> Bool {
+    return lhs.url == rhs.url
+        && lhs.method == rhs.method
+        && lhs.jsonBody as NSDictionary == rhs.jsonBody
+}
---
+++ Tests/network/HTTPRequestTests.swift
@@ -0,0 +1,37 @@
+//
+//  HTTPRequestTests.swift
+//
+//
+//
+
+import XCTest
+@testable import xcsautobuild
+
+class HTTPRequestTests: XCTestCase {
+    
+    // MARK: - ==
+
+    func test_equals_shouldNotBeEqual_whenURLsAreDifferent() {
+        XCTAssertFalse(isEqual(url: "bad url"))
+    }
+
+    func test_equals_shouldNotBeEqual_whenMethodsAreDifferent() {
+        XCTAssertFalse(isEqual(method: .get))
+    }
+
+    func test_equals_shouldNotBeEqual_whenJSONBodyAreDifferent() {
+        XCTAssertFalse(isEqual(jsonBody: ["not": "there"]))
+    }
+
+    func test_equals_shouldBeEqual() {
+        XCTAssert(isEqual())
+    }
+
+    // MARK: - Helpers
+
+    func isEqual(url url: String = "url", method: HTTPMethod = .post, jsonBody: [String: AnyObject] = ["key": "value"]) -> Bool {
+        let request = HTTPRequest(url: url, method: method, jsonBody: jsonBody)
+        let other = HTTPRequest(url: "url", method: .post, jsonBody: ["key": "value"])
+        return request == other
+    }
+}
