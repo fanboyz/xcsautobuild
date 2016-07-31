ac8218757b5d85bb6df52fcd1cba4d5846040ea4
Sean Henry <hello@seanhenry.codes>
2016-07-31 19:07:44 +0100

    Handles deleted branches in interactor

--- Sources/app/branch_filter/interactor/BranchSyncingInteractor.swift
+++ Sources/app/branch_filter/interactor/BranchSyncingInteractor.swift
@@ -1,25 +1,29 @@
 //
-//  NewBranchFetchingInteractor.swift
+//  BranchSyncingInteractor.swift
 //
 //
 //
 
 import Foundation
 
-class NewBranchFetchingInteractor: Command {
+class BranchSyncingInteractor: Command {
 
     private let branchesDataStore: BranchesDataStore
     private let botCreator: BotCreator
+    private let botDeleter: BotDeleter
 
-    init(branchesDataStore: BranchesDataStore, botCreator: BotCreator) {
+    init(branchesDataStore: BranchesDataStore, botCreator: BotCreator, botDeleter: BotDeleter) {
         self.branchesDataStore = branchesDataStore
         self.botCreator = botCreator
+        self.botDeleter = botDeleter
     }
 
     func execute() {
         branchesDataStore.load()
         let newBranches = branchesDataStore.getNewBranches()
+        let deletedBranches = branchesDataStore.getDeletedBranches()
         branchesDataStore.commit()
         newBranches.forEach { botCreator.createBot(forBranch: $0) }
+        deletedBranches.forEach { botDeleter.deleteBot(forBranch: $0) }
     }
 }
--- Tests/app/branch_filter/interactor/BranchSyncingInteractorTests.swift
+++ Tests/app/branch_filter/interactor/BranchSyncingInteractorTests.swift
@@ -1,5 +1,5 @@
 //
-//  NewBranchFetchingInteractorTests.swift
+//  BranchSyncingInteractorTests.swift
 //
 //
 //
@@ -7,17 +7,19 @@
 import XCTest
 @testable import xcsautobuild
 
-class NewBranchFetchingInteractorTests: XCTestCase {
+class BranchSyncingInteractorTests: XCTestCase {
     
-    var interactor: NewBranchFetchingInteractor!
+    var interactor: BranchSyncingInteractor!
     var mockedDataStore: MockBranchesDataStore!
     var mockedBotCreator: MockBotCreator!
+    var mockedBotDeleter: MockBotDeleter!
 
     override func setUp() {
         super.setUp()
         mockedDataStore = MockBranchesDataStore()
         mockedBotCreator = MockBotCreator()
-        interactor = NewBranchFetchingInteractor(branchesDataStore: mockedDataStore, botCreator: mockedBotCreator)
+        mockedBotDeleter = MockBotDeleter()
+        interactor = BranchSyncingInteractor(branchesDataStore: mockedDataStore, botCreator: mockedBotCreator, botDeleter: mockedBotDeleter)
     }
     
     // MARK: - execute
@@ -45,10 +47,28 @@ class NewBranchFetchingInteractorTests: XCTestCase {
         XCTAssertEqual(result, newBranches)
     }
 
+    func test_execute_shouldGetDeletedBranches() {
+        interactor.execute()
+        XCTAssert(mockedDataStore.didGetDeletedBranches)
+    }
+
+    func test_execute_shouldDeleteBotForEachBranch() {
+        let deletedBranches = ["develop", "feature/123"]
+        stubDeletedBranchNames(deletedBranches)
+        interactor.execute()
+        let result = mockedBotDeleter.invokedBranches.map { $0.name }
+        XCTAssertEqual(result, deletedBranches)
+    }
+
     // MARK: - Helpers
 
     func stubNewBranchNames(names: [String]) {
         let branches = names.map { Branch(name: $0) }
         mockedDataStore.stubbedNewBranches = branches
+    }
+
+    func stubDeletedBranchNames(names: [String]) {
+        let branches = names.map { Branch(name: $0) }
+        mockedDataStore.stubbedDeletedBranches = branches
     }
 }
